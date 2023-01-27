4b67f8307b397359844aa23eb475d0ec36434be9
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-01-27 16:44:36 +0100

    errores corregidos, resta ordenar favoritos segun lo que el usuario desee y finalizar maquetacion responsive

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
-import { getPhotoApi } from '../features/photosSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addPhoto } from '../features/favoritePhotosSlice';
-import { ErrorComponent } from './error';
+
 
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -10,41 +9,32 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
-import CircularProgress from '@mui/material/CircularProgress';
 
 
-export const ImageComponent = ({ query, page }) => {
+export const ImageComponent = (props) => {
 
-    // Importamos del estado la lista de fotos traidas de la API
-    const { list, loading, error } = useSelector(state => state.photos)
     const dispatch = useDispatch();
-    
-    useEffect(() => {
-        dispatch(getPhotoApi(query, page))
-    }, [query, page, dispatch])
 
     // Importamos acction para agregar a favoritos
     const today = new Date();
     const date = today.toLocaleString();
 
     const addPhotoFav = () => {
-        list.map(item => (
-            dispatch(addPhoto({
-                id: item.id,
-                name: item.user.name,
-                description: item.alt_description,
-                src: item.urls.regular,
-                likes: item.likes,
-                width: item.width,
-                height: item.height,
-                donwload: item.urls.small_s3,
-                date: date
-            }))
-        ))
+        dispatch(addPhoto({
+            id: props.id,
+            name: props.name,
+            description: props.description,
+            src: props.src,
+            likes: props.likes,
+            width: props.width,
+            height: props.height,
+            donwload: props.donwload,
+            date: date
+        }))
     };
 
     // Confirmacion de guardado en favoritos 
-    const [ confirm, setConfirm] = useState(false);
+    const [ confirm, setConfirm ] = useState(false);
 
     const handleFavorite = () => {
         setConfirm(true)
@@ -61,61 +51,46 @@ export const ImageComponent = ({ query, page }) => {
         }
     }, [confirm])
 
-    // Mostramos el return correspondiente al Status
-    if (loading && !error) {
-        return (
-            <div className='loading'>
-                <CircularProgress color="info" size={150} />
-            </div>
-        )
-    } else if (error && !loading) {
-        return <ErrorComponent />
-    } else {
-        return (
-            <section className='images'>
-                <ImageList cols={5} style={{gap: 10}}>
-    
-                    {list.map((item) => (
-    
-                        <ImageListItem key={item.id}>
-    
-                            <img
-                                src={`${item.urls.regular}?w=248&fit=crop&auto=format`}
-                                srcSet={`${item.urls.regular}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={item.user.name}
-                                loading="lazy"
-                                style={{borderRadius: 20}}
-                            />
-    
-                            <ImageListItemBar
-                                sx={{
-                                    background:
-                                    'linear-gradient(to top, rgba(0,0,0,0.7) 0%, ' +
-                                    'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-                                    borderRadius: '0px 0px 20px 20px'
-                                }}
-                                actionIcon={
-                                    <IconButton
-                                    sx={{ color: 'red' }}
-                                    onClick={handleFavorite}
-                                    >
-                                    <FavoriteIcon />
-                                    </IconButton>
-                                }
-                            />
-                        </ImageListItem>
-                    ))}
-    
-                </ImageList>
-                
-                {confirm && 
-                    <div className='save__fav'>
-                        <span>SAVED PHOTO</span>
-                        <DoneAllIcon sx={{color: '#6EC7B3', fontSize: 60}}/>
-                    </div>
-                }
-                
-            </section>
-        );
-    }
+
+    return (
+        <section className='images'>
+            <ImageList cols={5} style={{gap: 10}}>
+                <ImageListItem>
+
+                    <img
+                        src={`${props.src}?w=248&fit=crop&auto=format`}
+                        srcSet={`${props.src}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        alt={props.name}
+                        loading="lazy"
+                        style={{borderRadius: 20}}
+                    />
+
+                    <ImageListItemBar
+                        sx={{
+                            background:
+                            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, ' +
+                            'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                            borderRadius: '0px 0px 20px 20px'
+                        }}
+                        actionIcon={
+                            <IconButton
+                            sx={{ color: 'red' }}
+                            onClick={handleFavorite}
+                            >
+                            <FavoriteIcon />
+                            </IconButton>
+                        }
+                    />
+                </ImageListItem>
+            </ImageList>
+            
+            {confirm && 
+                <div className='save__fav'>
+                    <span>SAVED PHOTO</span>
+                    <DoneAllIcon sx={{color: '#6EC7B3', fontSize: 60}}/>
+                </div>
+            }
+            
+        </section>
+    );
 }
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ImageComponent } from "../components/images";
+import { ErrorComponent } from "../components/error";
+import { getPhotoApi } from '../features/photosSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Pagination from '@mui/material/Pagination';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
 import ClearIcon from '@mui/icons-material/Clear';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 export const SearchComponent = () => {
-
+    
     const [ input, setInput ] = useState('');
 
     const handleChangeInput = ({target}) => {
@@ -20,6 +26,14 @@ export const SearchComponent = () => {
     const handleChangePage = (e, value) => {
         setPage(value);
     }
+
+    // Importamos del estado la lista de fotos traidas de la API
+    const { list, loading, error } = useSelector(state => state.photos)
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(getPhotoApi(input, page))
+    }, [input, page, dispatch])
     
     return (
         <div>
@@ -57,10 +71,29 @@ export const SearchComponent = () => {
                     <Button onClick={() => setInput('cities')} variant="contained">CITIES</Button>
                     <Button onClick={() => setInput('colors')} variant="contained">COLORS</Button>
                 </div>
-
-                <div className="search__images">
-                    <ImageComponent query={input} page={page} />
-                </div>
+                
+                { loading && !error ? 
+                    <div className='loading'>
+                        <CircularProgress color="info" size={150} />
+                    </div>
+                    : !loading && error ?
+                    <ErrorComponent />
+                    :
+                    <div className="search__images">
+                        {list.map((item) => (
+                            <ImageComponent key={item.id} 
+                                id={item.id}
+                                name={item.user.name}
+                                description={item.alt_description}
+                                src={item.urls.regular}
+                                likes={item.likes}
+                                width={item.width}
+                                height={item.height} 
+                                donwload={item.urls.small_s3}
+                            />
+                        ))}
+                    </div>
+                }
 
                 <div className="search__pages">
                     <Pagination page={page} onChange={handleChangePage} count={10} variant="outlined" color="primary" />
